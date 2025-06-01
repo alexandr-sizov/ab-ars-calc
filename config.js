8ddbf2008d65b2541962de8d63874480117339de
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-06-02 01:37:04 +0200

    redux init

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const stepLength = 1;
 
 const wagonLength = 19.16;
-const wagonCount = 5;
+const wagonCount = 6;
 
-const interval = 90;
+const interval = Math.round(3600 / 44);
 
 const trainHalf = wagonLength * wagonCount / 2;
 
 const FT = -111.6 * 1.1;
-let KS = 1.5;
+let KS = 1;
 
-const abSteps = ['y','yg','g'];
-const arsSteps = [40,60,70,80];
+const abSteps = ['y', 'yg', 'g'];
+const arsSteps = [40, 60, 70, 80];
 // const arsSteps = [40];
